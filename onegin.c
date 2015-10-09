2369bdca36d3d396b4ad9fe9b25f685b5b897f70
Ahan05 <sha-ahan@yandex.ru>
2015-10-09 03:32:24 +0300

    Onegin

---
+++ onegin.c
@@ -0,0 +1,60 @@
+//
+//  main.c
+//  Onegin2
+//
+//  Created by Ahan Shabanov on 09.10.15.
+//  Copyright © 2015 Ahan Shabanov. All rights reserved.
+//
+
+#include <stdio.h>
+
+void sort(char** mass, const int n);
+
+int main()
+{
+    char stish[256][256],p, *ukaz[256];
+    int i,j = 0,n;
+    scanf("%d", &n);
+    gets(&p);
+    
+    
+    for (i=0; i < n; i++)
+    {
+        for (j=0; (stish[i][j] = getchar()) != '\n';j++ )
+        {
+            if (j == 0) ukaz[i] = &stish[i][j];
+        }
+    }
+    
+
+    sort(ukaz, n);
+    
+    for (i=0; i < n; i++)
+     printf("%s", ukaz[i]);
+    
+    
+    return 0;
+}
+
+
+void sort(char** mass, const int m)  //mass-массив  m-количество элементов
+{
+    int t,j,ch;
+    
+    t = 0;
+    while (t<(m-1))
+    {
+        if (*mass[t] > *mass[t+1])
+        {
+            ch = *mass[t];
+            *mass[t] = *mass[t+1];
+            *mass[t+1] = ch;
+            j = t+1;
+            if ((t-1) != -1)
+                t = t-1;
+        }
+        else
+            ++t;
+        
+    }
+}
