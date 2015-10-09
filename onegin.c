89ef948129e8b6c694b30b6483a8b7334891e495
Ahan05 <sha-ahan@yandex.ru>
2015-10-09 13:09:33 +0300

    Onegin(at last finished)

--- onegin.c
+++ onegin.c
@@ -27,13 +27,13 @@ int main()
         }
     }
     
-
+    
     sort(ukaz, n);
     
     printf("Отсортированный массив:\n");
     
     for (i=0; i < n; i++)
-     printf("%s", ukaz[i]);
+        printf("%s", ukaz[i]);
     
     
     return 0;
@@ -42,17 +42,18 @@ int main()
 
 void sort(char** mass, const int m)  //mass-массив  m-количество строк
 {
-    int t,j,ch;
+    int t;
+    char *ch;
     
     t = 0;
     while (t<(m-1))
     {
         if (*mass[t] > *mass[t+1])
         {
-            ch = *mass[t];
-            *mass[t] = *mass[t+1];
-            *mass[t+1] = ch;
-            j = t+1;
+            ch = mass[t];
+            mass[t] = mass[t+1];
+            mass[t+1] = ch;
+            //j = t+1;
             if ((t-1) != -1)
                 t = t-1;
         }
